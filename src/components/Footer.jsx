c7ee63f4f75f2940d1ee4e5bbc1c88312c61b1ef
Alicia Myrie <aliciamyrie@gmail.com>
2022-08-08 15:17:55 -0400

    added login abd logout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2022 Alicia Myrie</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Menubar({ name, setName }) {
+  return (
+    <nav style={{ display: "flex", justifyContent: "space-between" }}>
+      <span>Menubar</span>
+      {name ? (
+        <button onClick={() => setName(null)}>Logout</button>
+      ) : (
+        <button onClick={() => setName("Cisco")}>Login</button>
+      )}
+    </nav>
+  );
+}
+
+export default Menubar;
